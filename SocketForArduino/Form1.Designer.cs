1dd30fec931f7ef08623e1a0bc0b2c5e44c1ad92
XYphrodite <72796194+XYphrodite@users.noreply.github.com>
2022-07-11 14:16:41 +0300

    Добавьте файлы проекта.

---
+++ SocketForArduino/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SocketForArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.listenButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartX = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(86, 11);
+            this.portTextBox.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(180, 33);
+            this.portTextBox.TabIndex = 1;
+            // 
+            // listenButton
+            // 
+            this.listenButton.Location = new System.Drawing.Point(281, 8);
+            this.listenButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listenButton.Name = "listenButton";
+            this.listenButton.Size = new System.Drawing.Size(147, 45);
+            this.listenButton.TabIndex = 2;
+            this.listenButton.Text = "Start listening";
+            this.listenButton.UseVisualStyleBackColor = true;
+            this.listenButton.Click += new System.EventHandler(this.listenButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 101);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(612, 460);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(22, 571);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(612, 45);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status bar:";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(650, 17);
+            this.chart1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.Color = System.Drawing.Color.Maroon;
+            series1.Legend = "Legend1";
+            series1.Name = "Расстояние между точками";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1240, 930);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // chartX
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartX.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartX.Legends.Add(legend2);
+            this.chartX.Location = new System.Drawing.Point(22, 626);
+            this.chartX.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.chartX.Name = "chartX";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "X";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Y";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Z";
+            this.chartX.Series.Add(series2);
+            this.chartX.Series.Add(series3);
+            this.chartX.Series.Add(series4);
+            this.chartX.Size = new System.Drawing.Size(612, 321);
+            this.chartX.TabIndex = 7;
+            this.chartX.Text = "X";
+            this.chartX.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1904, 961);
+            this.Controls.Add(this.chartX);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listenButton);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Button listenButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartX;
+    }
+}
+
---
+++ SocketForArduino/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Windows.Forms;
+using SimpleTCP;
+
+namespace SocketForArduino
+{
+    public partial class Form1 : Form
+    {
+        bool lB = false;
+        int oldX=0, oldY=0, oldZ = 0;
+        bool isFirst = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpServer server;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;
+            server.StringEncoder = System.Text.Encoding.UTF8;
+            richTextBox1.ReadOnly = true;
+        }
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            richTextBox1.Invoke((MethodInvoker)delegate ()
+            {
+                chartX.Series["X"].Points.Clear();
+                chartX.Series["Y"].Points.Clear();
+                chartX.Series["Z"].Points.Clear();
+                String digit=String.Empty;
+                int x, y, z;
+                for (int i = 0; i < 4; i++)
+                {
+                    digit += e.MessageString[i];
+                }
+                x=decode(digit);
+                digit = String.Empty;
+                for (int i = 4; i < 8; i++)
+                {
+                    digit += e.MessageString[i];
+                }
+                y = decode(digit);
+                digit = String.Empty;
+                for (int i = 8; i < 12; i++)
+                {
+                    digit += e.MessageString[i];
+                }
+                z = decode(digit);
+                richTextBox1.Text += "Recieved at " + DateTime.Now.ToString("h:mm:ss tt") + 
+                ":\tX = " + x.ToString() + ", Y = " + y.ToString() + ", Z = " + z.ToString() + Environment.NewLine;
+                chartX.Series["X"].Points.Add(x);
+                chartX.Series["Y"].Points.Add(y);
+                chartX.Series["Z"].Points.Add(z);
+                if (!isFirst)
+                {
+                    chart1.Series[0].Points.Add(getDisance(oldX, oldY, oldZ, x, y, z));
+                    
+                }
+                else
+                {
+                    isFirst = false;
+                }
+                oldX = x;
+                oldY = y;
+                oldZ = z;
+            });
+        }
+
+        private void listenButton_Click(object sender, EventArgs e)
+        {
+            if (!lB)
+            {
+                System.Net.IPAddress ip = System.Net.IPAddress.Parse("127.0.0.1");
+                server.Start(ip, int.Parse(portTextBox.Text));
+                listenButton.Text = "Stop listening";
+                richTextBox1.Text += "Server is running...\n";
+                server.DataReceived += Server_DataReceived;
+            }
+            else
+            {
+                server.Stop();
+                listenButton.Text = "Start listening";
+                richTextBox1.Text += "Server was stopped\n";
+            }
+            lB = !lB;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            chartX.Series["X"].Points.Clear();
+            chartX.Series["Y"].Points.Clear();
+            chartX.Series["Z"].Points.Clear();
+            chart1.Series[0].Points.Clear();
+        }
+
+        private int decode(String a)
+        {
+            bool minus = false;
+            int result = 0;
+            if (a[0]=='p')
+            {
+
+            }
+            else if (a[0]=='m')
+            {
+                minus = true;
+            }
+            else
+            {
+                return 0;
+            }
+            switch (a[1])
+            {
+                case 'a':
+                    result += 100;
+                    break;
+                case 'b':
+                    result += 200;
+                    break;
+                case 'c':
+                    result += 300;
+                    break;
+                case 'd':
+                    result += 400;
+                    break;
+                case 'e':
+                    result += 500;
+                    break;
+                case 'f':
+                    result += 600;
+                    break;
+                case 'g':
+                    result += 700;
+                    break;
+                case 'h':
+                    result += 800;
+                    break;
+                case 'i':
+                    result += 900;
+                    break;
+                case 'v':
+                    break;
+            }
+            switch (a[2])
+            {
+                case 'a':
+                    result += 10;
+                    break;
+                case 'b':
+                    result += 20;
+                    break;
+                case 'c':
+                    result += 30;
+                    break;
+                case 'd':
+                    result += 40;
+                    break;
+                case 'e':
+                    result += 50;
+                    break;
+                case 'f':
+                    result += 60;
+                    break;
+                case 'g':
+                    result += 70;
+                    break;
+                case 'h':
+                    result += 80;
+                    break;
+                case 'i':
+                    result += 90;
+                    break;
+                case 'v':
+                    break;
+            }
+            switch (a[3])
+            {
+                case 'a':
+                    result += 1;
+                    break;
+                case 'b':
+                    result += 2;
+                    break;
+                case 'c':
+                    result += 3;
+                    break;
+                case 'd':
+                    result += 4;
+                    break;
+                case 'e':
+                    result += 5;
+                    break;
+                case 'f':
+                    result += 6;
+                    break;
+                case 'g':
+                    result += 7;
+                    break;
+                case 'h':
+                    result += 8;
+                    break;
+                case 'i':
+                    result += 9;
+                    break;
+                case 'v':
+                    break;
+            }
+            if (minus)
+            {
+                result = -result;
+            }
+            return result;
+        }
+
+        private double getDisance(int x1,int y1, int z1, int x2, int y2, int z2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+        }
+    }
+}
